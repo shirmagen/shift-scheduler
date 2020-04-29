4246c849f15d859296dbca85027d67160fa37059
Shirma <shirmagen095@gmail.com>
2020-04-30 00:49:21 +0300

    feat(one-month-shifts): added isSenior calcand empty worker representation
    *there must be at least one senior in shift
    *empty workers id is 0

--- src/index.js
+++ src/index.js
@@ -3,19 +3,45 @@ import _ from 'lodash'
 import {workers} from './workers';
 import {month} from './schedule';
 
-const getWorkerOnDay = dayNumber => {
-    const optionalWorkers = workers.filter(x => !(_.includes(x.constraints, dayNumber)) && x.shiftsAMonth > 0);
+const getTwoWorkersOnDay = dayNumber => {
+    const optionalWorkers = workers.filter(x => !(_.includes(x.constraints, dayNumber)) && x.shiftsAMonth > 0 && x.isSenior == 1);
     const optionalThatPrefer = optionalWorkers.filter(x => !(_.includes(x.preferNot, dayNumber)));
+    const firstWorker = optionalThatPrefer > 0 ? getWorkerFromOptions(optionalThatPrefer) : getWorkerFromOptions(optionalWorkers)
+    if(firstWorker == null)
+    {
+        return {first: {id: 0}, second: {id:0}};
+    }
 
-    const worker = optionalThatPrefer > 0 ? getWorkerFromOptions(optionalThatPrefer) : getWorkerFromOptions(optionalWorkers)
-    updateWorkerAfterPlaced({dayNumber, worker})
+    updateWorkerAfterPlaced({dayNumber, worker: firstWorker});
+    const secondWorker = getSecondWorker(firstWorker);
+    updateWorkerAfterPlaced({dayNumber, worker: secondWorker});
 
-    return worker;
+    return {first: firstWorker, second: secondWorker};
 };
 
 const getWorkerFromOptions = options => _.maxBy(options, x => x.shiftsAMonth);
+const getSecondWorker = ({id, shiftsAMonth, gender}) => {
+    let secondWorkerOptions = [];
+    while(secondWorkerOptions.length == 0 && shiftsAMonth > 0)
+    {
+        secondWorkerOptions = workers.filter(x => x.id != id && x.shiftsAMonth == shiftsAMonth && x.gender == gender);
+        shiftsAMonth--;
+    }
 
-const updateConstraints = ({dayNumber, worker}) => { worker.constraints.push(dayNumber + 1)};
+    if(shiftsAMonth == 0)
+    {
+        return {id :0};
+    }
+
+    const secondWorkerJunior =  secondWorkerOptions.find(x => x.isSenior == 0);
+    const secondWorker = secondWorkerJunior != null ? secondWorkerJunior : _.head(secondWorkerOptions);
+    
+    return secondWorker;
+}
+
+const updateConstraints = ({dayNumber, worker}) => { 
+    worker.constraints.push(dayNumber + 1)
+};
 const updatePreferNot = ({dayNumber, worker}) => {
     if(worker.shiftsAMonth < 4)
     {
@@ -28,12 +54,16 @@ const updatePreferNot = ({dayNumber, worker}) => {
 const updateShiftsAMonth = worker => {worker.shiftsAMonth--};
 
 const updateWorkerAfterPlaced = ({dayNumber, worker}) => {
-    updateConstraints({dayNumber, worker});
-    updatePreferNot({dayNumber, worker});
-    updateShiftsAMonth(worker);
+    if(worker.id != 0)
+     updateConstraints({dayNumber, worker});
+     updatePreferNot({dayNumber, worker});
+     updateShiftsAMonth(worker);
 };
 
-const shifts = month.map(({number}) => getWorkerOnDay(number));
-month.forEach(day => day.worker = shifts[day.number - 1].id);
+const shifts = month.map(({number}) => getTwoWorkersOnDay(number));
+month.forEach(day => {
+    day.firstWorker = shifts[day.number - 1].first.id;
+    day.secondWorker = shifts[day.number - 1].second.id;
+});
 
 console.log(month);
