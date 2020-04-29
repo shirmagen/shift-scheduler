c9c205998a5d28fb85b0a00cfe7f1eea8bac3963
Shirma <shirmagen095@gmail.com>
2020-04-30 00:09:35 +0300

    feat(one-month-shifts): added preferNot

--- src/index.js
+++ src/index.js
@@ -5,15 +5,35 @@ import {month} from './schedule';
 
 const getWorkerOnDay = dayNumber => {
     const optionalWorkers = workers.filter(x => !(_.includes(x.constraints, dayNumber)) && x.shiftsAMonth > 0);
-    const worker = _.head(optionalWorkers);
-    worker.constraints.push(dayNumber + 1);
-    worker.shiftsAMonth--;
+    const optionalThatPrefer = optionalWorkers.filter(x => !(_.includes(x.preferNot, dayNumber)));
+
+    const worker = optionalThatPrefer > 0 ? getWorkerFromOptions(optionalThatPrefer) : getWorkerFromOptions(optionalWorkers)
+    updateWorkerAfterPlaced({dayNumber, worker})
 
     return worker;
 };
 
-const shifts = month.map(({number}) => getWorkerOnDay(number));
+const getWorkerFromOptions = options => _.maxBy(options, x => x.shiftsAMonth);
 
+const updateConstraints = ({dayNumber, worker}) => { worker.constraints.push(dayNumber + 1)};
+const updatePreferNot = ({dayNumber, worker}) => {
+    if(worker.shiftsAMonth < 4)
+    {
+        for (let index = dayNumber; index < dayNumber + 7; index++) {
+            worker.preferNot.push(index);
+        };
+    }
+};
+
+const updateShiftsAMonth = worker => {worker.shiftsAMonth--};
+
+const updateWorkerAfterPlaced = ({dayNumber, worker}) => {
+    updateConstraints({dayNumber, worker});
+    updatePreferNot({dayNumber, worker});
+    updateShiftsAMonth(worker);
+};
+
+const shifts = month.map(({number}) => getWorkerOnDay(number));
 month.forEach(day => day.worker = shifts[day.number - 1].id);
 
 console.log(month);
